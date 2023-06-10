280142c67ca540d33eecd8a7648b6cc9d9d763c1
amarlong <98260184+AMarlonG@users.noreply.github.com>
2023-06-10 18:03:55 +0200

    Update testing.js

--- Taxes and benefits/testing.js
+++ Taxes and benefits/testing.js
@@ -1,13 +1,10 @@
 
 
 
-// function taxBracketstepOne() {
-//     if (myIncome > tax.bracket.stepOne.low && myIncome === tax.bracket.stepOne.low) {
-//         result = tax.bracket.stepOne.taxedSum
-//     }
-// }
+
 
 const myIncome = 650000;
+
 
 const taxBracket = (low, high, tax) => {
     return {
@@ -21,17 +18,30 @@ const taxBracket = (low, high, tax) => {
 
 };
 
-const stepOne = taxBracket(198350, 279150, 0.017);
-const stepTwo = taxBracket(279150, 642590, 0.04);
-const stepThree = taxBracket(642590, 926800, 0.134);
-const stepFour = taxBracket(926800, 1500000, 0.164);
-const stepFive = taxBracket(1500000, Infinity, 0.174);
+const steps = {
+    stepOne: taxBracket(198350, 279150, 0.017),
+    stepTwo: taxBracket(279150, 642590, 0.04),
+    stepThree: taxBracket(642590, 926800, 0.134),
+    stepFour: taxBracket(926800, 1500000, 0.164),
+    stepFive: taxBracket(1500000, Infinity, 0.174),
+};
 
-
-[stepOne, stepTwo, stepThree, stepFour, stepFive].forEach((item) => {
-    console.log(item);
-    
-});
+for (key in steps) {
+    const elm = steps[key];
+    console.log(
+        key +
+        ": " +
+        (() => {
+            if (myIncome === elm.high) {
+                return elm.sum();
+            } else if (myIncome > elm.low && myIncome < elm.high) {
+                return (myIncome - elm.low) * elm.tax;
+            } else if (myIncome < elm.low) {
+                return "Income is to low for bracket";
+            }
+        })()
+    );
+}
 
 
 
