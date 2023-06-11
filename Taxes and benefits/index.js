a311b31f29a0f772a049adce9c4b3ad4a5b07f2a
amarlong <98260184+AMarlonG@users.noreply.github.com>
2023-06-11 10:21:53 +0200

    Finished personal income and tax calculations

--- Taxes and benefits/index.js
+++ Taxes and benefits/index.js
@@ -72,14 +72,13 @@ First I build three basic calculators, then I build a calculator for Norwegian i
 
 const noDecimals = Math.trunc;
 
-// NATIONAL INSURANCE DATA
-
+// National insurance data
 const oneG = 118620 // Base calculation income
 const maxIncomeFullPension = oneG * 7.1 // Maximum income for full pension
 const maxIncomeFullBenefits = oneG * 6 // No benefit for income above this number
 const minIncomeForBenefits = oneG * 0.75 // Minimum income for receiveing benefits
 const myIncome = maxIncomeFullPension; // Max sum taken as income
-console.log('My income: ' + myIncome);
+console.log('Gross income: ' + myIncome);
 
 const myIncomeBrokenDown = {
 
@@ -103,13 +102,13 @@ const myIncomeBrokenDown = {
         return noDecimals(myIncome / this.hours);
     },
 };
-
 let monthlyIncome = myIncomeBrokenDown.monthly();
 let weeklyIncome = myIncomeBrokenDown.weekly();
 let dailyIncome = myIncomeBrokenDown.daily();
 let hourlyIncome = myIncomeBrokenDown.hourly();
 
-const vacation = {
+// Vacation pay
+const vacationInfo = {
     percentOfIncome: 0.12,
     daysAllowed: 30,
     daysWithPay: 26,
@@ -118,34 +117,36 @@ const vacation = {
         return noDecimals(monthlyIncome + (myIncome * this.percentOfIncome) - (monthlyIncome / this.daysWithPay) * this.daysAllowed)
     }
 };
-const vacationPayment = vacation.payment();
-console.log('My vacation pay: ' + vacationPayment);
+const vacationPayment = vacationInfo.payment();
+console.log('Vacation pay: ' + vacationPayment);
 
+// Tax deductions
 const taxDeductions = {
     minimum: 109950,
     individual: 58250
 };
 const totalTaxDeductions = taxDeductions.minimum + taxDeductions.individual;
-console.log('My total deductions: ' + totalTaxDeductions);
+console.log('Total deductions: ' + totalTaxDeductions);
 
 const myIncomeAfterTaxDeductions = myIncome - totalTaxDeductions;
-console.log('My income after deductions: ' + myIncomeAfterTaxDeductions);
+console.log('Income after deductions: ' + myIncomeAfterTaxDeductions);
 
-const tax = {
-    income: 0.22,
-    income() {
-        return myIncomeAfterTaxDeductions * this.income;
-    },
-
-    nationalInsurance: 0.08,
-    nationalInsurance() {
-        return myIncome * this.nationalInsurance;
-    },
+// Income tax (net income)
+const incomeTaxPercent = 0.22;
+incomeTax = () => {
+    return noDecimals(myIncomeAfterTaxDeductions * incomeTaxPercent);
 };
+console.log('Income tax: ' + incomeTax());
 
-console.log(tax.nationalInsurance());
+// National insurance tax (gross income)
+const nationalInsuranceTaxPercent = 0.08;
+nationalInsuranceTax = () => {
+    return noDecimals(myIncome * nationalInsuranceTaxPercent);
+};
+console.log('National insurance tax: ' + nationalInsuranceTax());
 
-// Tax bracket calculations taken from: https://stackoverflow.com/questions/63231845/how-to-calculate-effective-tax-rate-in-javascript/76447211#76447211
+// Backet tax (gross income)
+// Calculations used https://stackoverflow.com/questions/63231845/how-to-calculate-effective-tax-rate-in-javascript/76447211#76447211
 const allTaxBrackets = [
     [198350, 279150, 0.017],
     [279150, 642590, 0.04],
@@ -157,16 +158,26 @@ const allTaxBrackets = [
 let totalBracketTaxes = 0;
 for (const brackets of allTaxBrackets) {
     if (myIncome < brackets[1]) {
-        totalBracketTaxes += (myIncome - brackets[0]) * brackets[2];
+        totalBracketTaxes += noDecimals((myIncome - brackets[0]) * brackets[2]);
         break;
     }
     else {
-        totalBracketTaxes += (brackets[1] - brackets[0]) * brackets[2];
+        totalBracketTaxes += noDecimals((brackets[1] - brackets[0]) * brackets[2]);
     };
 };
+console.log('Bracket tax: ' + totalBracketTaxes);
 
-console.log(totalBracketTaxes);
+// Total taxes
+const allTaxesTotal = incomeTax() + nationalInsuranceTax() + totalBracketTaxes;
+console.log('Total taxes paid: ' + allTaxesTotal);
 
+// Net income
+const myNetIncome = myIncome - allTaxesTotal;
+console.log('Net income: ' + myNetIncome);
+
+// Tax percent
+const finalTaxPercentOfGrossIncome = noDecimals((allTaxesTotal * 100) / myIncome);
+console.log('Tax percent of gross income: ' + finalTaxPercentOfGrossIncome);
 
 
 
